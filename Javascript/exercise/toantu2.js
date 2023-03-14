139fbf619d637e2be6c8cc6ef7f31e8cf399559b
NhatDuy98 <dangnhatduy98@gmail.com>
2023-03-14 16:03:00 +0700

    do toantu2

---
+++ Javascript/exercise/toantu2.js
@@ -0,0 +1,11 @@
+let inputWidth
+inputWidth = prompt('Nhập chiều dài: ')
+let inputHeight
+inputHeight = prompt('Nhập chiều cao: ')
+
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+
+let area = width * height
+
+document.write('Area =' + area)
