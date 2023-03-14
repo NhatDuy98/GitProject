acc0da7c5a6a4e3f65e26a64a72ebbb2586fd53a
NhatDuy98 <dangnhatduy98@gmail.com>
2023-03-14 09:38:49 +0700

    do exercise java

---
+++ Javascript/exercise/javaex.js
@@ -0,0 +1,13 @@
+
+function showME(){
+    alert('Xin chào');
+}
+function showMessage(){
+    document.write('Hello')
+}
+function showMess(){
+    document.getElementById('hello').innerHTML='Hello'
+}
+function showM(){
+    console.log('Xin chào')
+}
