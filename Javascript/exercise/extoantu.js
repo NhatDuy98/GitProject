6d0a326ba7fa05783cb0be7f4b60986bef84acf3
NhatDuy98 <dangnhatduy98@gmail.com>
2023-03-15 09:02:10 +0700

    do ex toantu

---
+++ Javascript/exercise/extoantu.js
@@ -0,0 +1,38 @@
+/*let a
+inputa = prompt('Nhập điểm Vật lý: ')
+let b
+inputb = prompt('Nhập điểm Hóa học: ')
+let c
+inputc = prompt('Nhập điểm Sinh học: ')
+
+let vl = parseInt(inputa)
+let hh = parseInt(inputb)
+let sh = parseInt(inputc)
+ let sm = vl + hh + sh
+ let tb = ( vl + hh + sh)/3
+ document.write('Tổng điểm là:' + sm)
+ document.write('<br>') 
+ document.write('Điểm trung bình là: ' + tb)
+ document.write('<br>') */
+
+ /*let z 
+ inputz = prompt ('Nhập độ C: ')
+ let dc = parseInt (inputz)
+ let df = (dc/5)*9+32
+ document.write('Độ C chuyển sang độ F là: ' + df + ' độ F') */
+
+ /*const pi = 3.14
+ let q
+ inputq = prompt('Nhập bán kính hình tròn: ')
+ let bk = parseInt(inputq)
+ let cv = (bk*2)*pi
+ let dt = bk*bk*pi
+ document.write('Chu vi hình tròn là: ' + cv)
+ document.write('<br>')
+ document.write('Diện tích hình tròn là: ' + dt)*/
+
+ 
+ 
+
+
+ 
