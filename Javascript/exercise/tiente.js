babe01830e715d82155ecf80ea7517c0028c7d87
NhatDuy98 <dangnhatduy98@gmail.com>
2023-03-15 15:35:56 +0700

    do tiente

--- Javascript/exercise/tiente.js
+++ Javascript/exercise/tiente.js
@@ -1,7 +1,20 @@
-let a = document.getElementById('amount').value
-let from = document.getElementById('from').value
-let to = document.getElementById('to').value
-let d = document.getElementById('btn')
+function handleClick() {
+    console.log("aaaaaaaaa");
+    let a = document.getElementById('amount').value;
+    let from = document.getElementById('from').value;
+    let to = document.getElementById('to').value;
+    let result;
+    
+    if(from == 'VND' && to == 'USD') {
+        result = 'Result: ' + (a*23000) + 'VND';
+    }
+    else {
+        result = 'Result: ' + (a/23000) + 'USD';
+    } 
+    document.getElementById('kq').innerHTML = result;
+}
+    
+
 
     
 
