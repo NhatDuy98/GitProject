3df739473b4ff0b794ee6527ca7f1c23589b49a4
NhatDuy98 <dangnhatduy98@gmail.com>
2023-03-16 14:10:56 +0700

    do employee

---
+++ Javascript/Loops/math3ngoi.js
@@ -0,0 +1,10 @@
+// let a = prompt(' Nhập số a:', 0)
+// let b = prompt(' Nhập số b:', 0)
+
+// document.write(`${ a + b < 4 ? "Below" : "Over"}`)
+
+let login = prompt('Login: ', '')
+let message = document.write(`${login == 'Employee' ? "Hello" : login == 'Director' ? "Greetings" : login == '' ? "No login" : ""}`);
+
+
+
