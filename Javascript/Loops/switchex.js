aad5a595e16b035b6f194aa6dbf481853b062014
NhatDuy98 <dangnhatduy98@gmail.com>
2023-03-16 11:38:07 +0700

    do ex switch

---
+++ Javascript/Loops/switchex.js
@@ -0,0 +1,111 @@
+let value = prompt('Viết 1 số: ', 0)
+
+
+
+
+if (value > 0) {
+    document.write('1')
+}
+else if (value <0) {
+    document.write('-1')
+}
+else {
+    document.write('0')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = prompt('a?', '');
+// switch (a) {
+//     case '0':
+//         +document.write('0')
+//         break;
+//     case '1':
+//         +document.write('1')
+//         break;
+//     case '2':
+//     case '3':
+//         +document.write('2,3')
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let browser = prompt ('Enter browser name: ')
+// switch (browser) {
+//     case 'Edge':
+//         document.write("You've got the Edge");
+//         break;
+//     case 'Chrome':
+//         document.write("Okay we support these browsers too")
+//         break;
+//     case 'Firefox':
+//         document.write("Okay we support these browsers too")
+//         break;
+//     case 'Safari':
+//         document.write("Okay we support these browsers too")
+//         break;
+//     case 'Opera':
+//         document.write("Okay we support these browsers too")
+//         break;
+
+//     default:
+//         document.write("We hope that this page looks ok!")
+//         break;
+// }
